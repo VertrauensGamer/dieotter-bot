0d3d57ff5ecdacf7b853b6e8251b9bf19dddb298
VertrauensGamer <eliasneumann77@gmail.com>
2024-08-19 21:37:58 +0200

    auto deletion of Database Collection on bot start(not for tickets)

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 import os
 import logging
 from cogs import ticket
+from pymongo import MongoClient
 
 logger = logging.getLogger("discord")
 logger.setLevel(logging.DEBUG)
@@ -16,6 +17,15 @@ bot = discord.Bot()
 
 @bot.event
 async def on_ready():
+    
+    client = MongoClient("mongodb://localhost:27017/")
+    db = client["DiscordBotDB"]
+    giveawaycol = db["GiveawayCollection"]
+    entrycol = db["GiveawayEntry"]
+    
+    entrycol.delete_many({})
+    giveawaycol.delete_many({})
+    
     print(f"{bot.user} ist online!")
     bot.add_view(ticket.OpenTicket())
     activity = discord.Game(name="Die Otter Discord")
