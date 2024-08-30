08610d41f7f37b3f3770846c291c67e0c0fccfbc
VertrauensGamer <eliasneumann77@gmail.com>
2024-08-30 21:43:39 +0200

    orginized main.py

--- main.py
+++ main.py
@@ -1,37 +1,32 @@
 import datetime
 import os
 import logging
+import time
 import discord
 from discord.ext import commands, tasks
 from dotenv import load_dotenv
 from pymongo import MongoClient
 from cogs import ticket
-import time
-
-START_TIME = time.time()
 
 # Load environment variables
 load_dotenv()
 
 # Constants
+START_TIME = time.time()
 MONGO_URI = "mongodb://localhost:27017/"
 DB_NAME = "DiscordBotDB"
 GIVEAWAY_COLLECTION = "GiveawayCollection"
 ENTRY_COLLECTION = "GiveawayEntry"
 TICKET_COLLECTION = "TicketCollection"
 COGS_LIST = [
-    'help',
-    'ticket',
-    'giveaway',
-    'announcement',
-    'oil',
-    'moderation',
-    'server_stats',
-    'music',
-    'feedback',
+    'help', 'ticket', 'giveaway', 'announcement', 'oil',
+    'moderation', 'server_stats', 'music', 'feedback',
 ]
 
-# Set up logging
+# Bot setup
+bot = discord.Bot(intents=discord.Intents.all())
+
+# Logging setup
 def setup_logging():
     logger = logging.getLogger("discord")
     logger.setLevel(logging.DEBUG)
@@ -60,6 +55,7 @@ def get_count_collection():
     db = client[DB_NAME]
     return db["CountCollection"]
 
+# Bot status and stats functions
 @tasks.loop(seconds=1)
 async def update_counts():
     server_count = len(bot.guilds)
@@ -71,23 +67,21 @@ async def update_counts():
         {"$set": {"server_count": server_count, "user_count": user_count, "bot_uptime": uptime}},
         upsert=True
     )
-    
+
 def update_bot_status(status):
     get_count_collection().update_one({"_id": "bot_stats"}, {"$set": {"status": str(status)}}, upsert=True)
-    
+
 def bot_uptime():
     current_time = time.time()
-    diffrence = int(round(current_time - START_TIME))
-    return str(datetime.timedelta(seconds=diffrence))
-    
+    difference = int(round(current_time - START_TIME))
+    return str(datetime.timedelta(seconds=difference))
+
 def clear_giveaway_data():
     giveawaycol, entrycol = get_db_collections()
     entrycol.delete_many({})
     giveawaycol.delete_many({})
 
-# Bot setup
-bot = discord.Bot(intents=discord.Intents.all())
-
+# Bot events
 @bot.event
 async def on_ready():
     global START_TIME
@@ -99,7 +93,6 @@ async def on_ready():
     bot.add_view(ticket.OpenTicket())
     activity = discord.Game(name="Die Otter Discord")
     await bot.change_presence(activity=activity)
-    
 
 @bot.event
 async def on_member_join(member: discord.Member):
@@ -107,16 +100,18 @@ async def on_member_join(member: discord.Member):
     if role:
         await member.add_roles(role)
         print(f"Assigned {role} to {member.name}")
-        
+
 @bot.event
 async def on_disconnect():
     update_bot_status("Offline")
     update_counts.stop()
 
+# Cog loading
 def load_cogs():
     for cog in COGS_LIST:
         bot.load_extension(f'cogs.{cog}')
 
+# Main function
 def main():
     setup_logging()
     load_cogs()
